a4fbd662219e83a88b776dcd4e9a31393b5076c0
amirhossein <amirhosseinghodratnema@gmail.com>
2023-10-04 18:28:05 +0330

    Fix

---
+++ backEnd/model/user.js
@@ -0,0 +1,11 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    lastName: { type: String, default: '' },
+    name: { type: String, default: '' },
+}, { timestamps: true });
+
+const User = models.User || model("User", UserSchema);
+export default User;
